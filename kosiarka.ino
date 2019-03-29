64bf207d1580072ce4b53cf4d35adaf0a2dab037
Aleksander <koniu95@interia.pl>
2019-03-29 21:56:40 +0100

    czujnik zjebany

--- kosiarka.ino
+++ kosiarka.ino
@@ -1,19 +1,13 @@
-#include <PWMServo.h>
 #include <TridentTD_7Segs74HC595.h>
 
 // * KONSTANTY *
-#define SERVO_MIN_ANGLE 70
-#define SERVO_MAX_ANGLE 140 
-#define GAZ_MIN 0
-#define GAZ_MAX 1023
+
 #define MIN_RPM 1000
 #define REFRESH_RATE 333
-#define SERVO_MIN_PULSE 600
-#define SERVO_MAX_PULSE 2600
+
 #define RESOLUTION 25
 
 //* PINY *
-#define SERVO 9
 #define CZUJNIK 3
 #define SCLK 5
 #define RCLK 6
@@ -21,12 +15,9 @@
 #define ENCODER_A 2
 #define ENCODER_B 4
 #define ENCODER_BUTTON 10
-const int GAZ = A1;
 
 // * GLOBALSY *
 TridentTD_7Segs74HC595  dsp( SCLK, RCLK, DIO, 2);
-//RBD::Servo gaz(SERVO,SERVO_MIN_PULSE,SERVO_MAX_PULSE);
-PWMServo gaz;
 volatile byte rpm_set_level = 0;
 volatile byte rpm_tick_counter = 0;
 bool mode = false; //true - zdalne, false - sterowanie samemu 
@@ -45,12 +36,13 @@ void enc()
 {  
     encTime = millis();
     if((diffTime = encTime - prev_time) >= 90){
-    if(digitalRead(ENCODER_B))
+    if(!digitalRead(ENCODER_B))
       rpm_set_level += diffTime<180?4:1;    
     else
       rpm_set_level -= diffTime<180?4:1;     
     prev_time = encTime;
     }
+    setChanged = true;
 }
  
 void tick()
@@ -62,34 +54,33 @@ void tick()
     sumTime += diffRpm;
 }
 
-bool firstTime;
+
 void setup()
-{    
-  //Serial.begin(9600);
+{  
   pinMode(CZUJNIK,INPUT_PULLUP);  
   pinMode(ENCODER_A,INPUT_PULLUP);
   pinMode(ENCODER_B,INPUT_PULLUP);
   //pinMode(ENCODER_BUTTON,INPUT);
-  pinMode(GAZ,INPUT);
+  
   dsp.addModule("Rpmreal");
   dsp.addModule("Rpmset");
   
   dsp.init();
   
   attachInterrupt(digitalPinToInterrupt(CZUJNIK),tick,FALLING);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A),enc,FALLING);
-  firstTime = true;
-  gaz.attach(SERVO);   
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A),enc,FALLING); 
 }
 byte x = 0;
 unsigned long refreshTime = 0;
-
+int lastGaz = 0;
+bool flagServo = true;
+int servoCounter = 0;
 void loop() 
-{  
+{          
         if(rpm_set_level != x)
         {
           x = rpm_set_level;
-          dsp.setNumber("Rpmset",MIN_RPM+rpm_set_level*RESOLUTION,0);       
+          dsp.setNumber("Rpmset",MIN_RPM+rpm_set_level*RESOLUTION,0);              
         }
 
         if(millis() - refreshTime >= REFRESH_RATE)
@@ -100,23 +91,14 @@ void loop()
             rpm = 0;  
           }
           else{
-            float avgTime = (float)sumTime/rpm_tick_counter;
-            rpm = 60000.0/avgTime/RESOLUTION*RESOLUTION;
+            float avgTime = (float)sumTime/(float)rpm_tick_counter;
+            rpm = (int)(60000.0/avgTime)/RESOLUTION*RESOLUTION;
           }
           rpm_tick_counter = 0;
           sumTime = 0;          
           interrupts();
           dsp.setNumber("Rpmreal",rpm,0);
           refreshTime = millis();  
-        }
+        }       
         
-        int gazPower = analogRead(GAZ);
-        if(!mode)
-        {       
-          int angle = map(gazPower,GAZ_MIN,GAZ_MAX,SERVO_MIN_ANGLE,SERVO_MAX_ANGLE);          
-          gaz.write(angle);    
-          delay(15);
-        }else{
-            
-        }
 }

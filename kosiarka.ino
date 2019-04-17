df9d5269a84e49fb52a2ce4f86edf4c0fb4c418e
Aleksander <koniu95@interia.pl>
2019-04-17 13:21:29 +0200

    mode wont run below 1000 rpm

--- kosiarka.ino
+++ kosiarka.ino
@@ -2,7 +2,8 @@
 
 // * KONSTANTY *
 #define MIN_RPM 1000
-#define REFRESH_RATE 500
+#define REFRESH_RATE 250
+#define LOOP_RATE 100
 
 #define RESOLUTION 50
 
@@ -15,11 +16,15 @@ const int CZUJNIK = A1;
 #define ENCODER_B 4
 #define ENCODER_BUTTON 3
 
+#define ACTION 7
+#define HIGHER 9
+#define MODE 10
+
 // * GLOBALSY *
 TridentTD_7Segs74HC595  dsp( SCLK, RCLK, DIO, 2);
 volatile byte rpm_set_level = 0;
 volatile int rpm_tick_counter = 0;
-bool mode = false; //true - zdalne, false - sterowanie samemu 
+volatile bool mode = false; //true - zdalne, false - sterowanie samemu 
 int angle = 0;
 long rpm = 0;
 volatile long prev_time = 0;
@@ -36,7 +41,7 @@ void enc()
     encTime = millis();
     if((diffTime = encTime - prev_time) >= 90){
     if(digitalRead(ENCODER_B)){      
-      if(rpm_set_level <251)
+      if(rpm_set_level < 251)
       rpm_set_level += diffTime<180?4:1;
     }          
     else{
@@ -45,6 +50,18 @@ void enc()
     }     
     prev_time = encTime;
     }    
+}
+
+unsigned volatile long previousMode = 0;
+void changeMode()
+{
+    unsigned long timeN = millis();
+    if(timeN - previousMode > 100 && rpm <= 1000L)
+    {
+      mode = !mode;
+      digitalWrite(MODE,mode);
+      previousMode = timeN;  
+    }  
 }
  
 //void tick()
@@ -63,13 +80,20 @@ void setup()
   pinMode(CZUJNIK,INPUT);  
   pinMode(ENCODER_A,INPUT_PULLUP);
   pinMode(ENCODER_B,INPUT_PULLUP);
-  pinMode(ENCODER_BUTTON,INPUT);
+  pinMode(ENCODER_BUTTON,INPUT_PULLUP);
+  pinMode(ACTION,OUTPUT);
+  pinMode(HIGHER,OUTPUT);
+  pinMode(MODE,OUTPUT);
+  digitalWrite(ACTION,LOW);
+  digitalWrite(HIGHER,LOW);
+  digitalWrite(MODE,LOW);
   
   dsp.addModule("Rpmreal");
   dsp.addModule("Rpmset");
   
   dsp.init();  
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A),enc,FALLING); 
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A),enc,FALLING);   
+  attachInterrupt(digitalPinToInterrupt(ENCODER_BUTTON),changeMode,RISING); 
 }
 byte x = 0;
 unsigned long refreshTime = 0;
@@ -82,6 +106,7 @@ int value = 0;
 byte count = 0;
 //long times[2] = {0,0};
 unsigned long timeNow = 0;
+unsigned long lastRPM = 0;
 void loop() 
 {          
         if(rpm_set_level != x)
@@ -92,37 +117,42 @@ void loop()
         value = analogRead(CZUJNIK);
         vout = (value * 5) / 1024.0;
         vin = vout / (R2 / (R1 + R2));
-        //Serial.println(vin);
-        if (vin < 50.)
+        
+        if (vin > 50.)
         {
-          if(micros() - timeNow >= REFRESH_RATE*500)rpm = 0;
-        } else {
           long rpm_pom = 100000000/(micros()-timeNow)/2;
-          if(rpm_pom < 4000)
-          rpm = rpm_pom;
-          rpm /= RESOLUTION;
-          rpm *= RESOLUTION;
+          if(rpm_pom < 4000){
+            rpm = rpm_pom / RESOLUTION;            
+            rpm *= RESOLUTION;
+            //if(lastRPM - rpm > 500)rpm = lastRPM;
+            //else lastRPM = rpm;
+          }
           timeNow = micros();    
         }
-
+        
         if(millis() - refreshTime >= REFRESH_RATE)
-        {
-          //noInterrupts();
-//          timeNow = micros();
-//          do{
-//            value = analogRead(CZUJNIK);
-//            vout = (value * 5) / 1024.0;
-//            vin = vout / (R2 / (R1 + R2));  
-//            if(vin > 50.){                      
-//            rpm = 60000000/(micros()-timeNow)/2;                                 
-//            timeNow = micros();
-//            }
-//          }while(rpm > 4000);
-//          rpm /= RESOLUTION;
-//          rpm *= RESOLUTION;
-          //interrupts();
+        {          
+          if(millis() - timeNow/1000 >= REFRESH_RATE)rpm = 0;
+          
+          //rpm = 2500; // TESTESTESTESTESTEST
+
+                              
+          if(mode)
+          {
+              noInterrupts();
+              long setLevel = MIN_RPM + rpm_set_level*RESOLUTION;
+              if(rpm - setLevel > 150L || rpm - setLevel < -150L)
+              {
+                 digitalWrite(ACTION,HIGH);
+                 digitalWrite(HIGHER,rpm<setLevel?HIGH:LOW); 
+              } else {
+                 digitalWrite(ACTION,LOW);  
+              }
+              interrupts();
+          } 
           dsp.setNumber("Rpmreal",rpm,0);
           refreshTime = millis();  
-        }       
+        } 
+
         
 }
